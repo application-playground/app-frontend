0e33af32bf3149fe47820c9bc9f7327d790a913b
Hardik Chauhan <hardikidea@gmail.com>
2019-08-28 17:51:32 +0530

    pagination-changes

--- src/app/Administrator/mat-table/mat-table.component.ts
+++ src/app/Administrator/mat-table/mat-table.component.ts
@@ -2,7 +2,6 @@ import { UserService } from 'src/app/services/user.service';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { PeriodicElement } from './PeriodicElement.model';
 import { BreadCrumb } from 'src/app/Model/breadcrumb.model';
 import { PageModel } from 'src/app/Model/page.model';
 
@@ -14,11 +13,10 @@ declare var jQuery: any;
 })
 export class MatTableComponent implements OnInit, AfterViewInit {
 
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-
+  MyDataSource: any;
   displayedColumns: string[] = ['name'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   breadCrumbSource: any[] = [];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   page: PageModel = new PageModel();
 
   constructor(private userService: UserService) {
@@ -26,64 +24,32 @@ export class MatTableComponent implements OnInit, AfterViewInit {
     this.breadCrumbSource.push(new BreadCrumb('Home', 'template', false));
     this.breadCrumbSource.push(new BreadCrumb('Gallery', '', false));
     this.breadCrumbSource.push(new BreadCrumb('Lightbox', '', true));
-   }
+
+    this.page.size = 20;
+    this.page.pageNumber = 1;
+  }
 
   ngOnInit() {
-
-    // this.page.size = 3;
-    // this.page.pageNumber = 0;
-    // this.page.totalElements = 20;
-
+    this.RenderDataTable();
   }
 
-  ngAfterViewInit() {
-    // this.paginator.pageSize = 3;
-    // this.paginator.pageSizeOptions = [1, 2, 3, 4, 5];
-    // this.paginator.pageIndex = 3;
-    this.dataSource.paginator = this.paginator;
-  }
-
-  getRequestData(ev: Event) {
-    console.log(ev);
-    // tslint:disable-next-line: no-string-literal
-    this.page.pageNumber = ev['pageIndex'] || 0;
-    // tslint:disable-next-line: no-string-literal
-    this.page.size = ev['pageSize'] || 0;
-    // tslint:disable-next-line: no-string-literal
-    this.page.totalElements = ev['length'] || 0;
+  RenderDataTable() {
     this.userService.getRegistrationData('users/list', this.page).subscribe((res) => {
 
-      
       if (!res['error']) {
-        this.paginator.pageSize = res['pages'] || 0;
-        this.page.totalElements = res['totalElement'] || 0;
-        console.log(res['message']);        
-        this.dataSource = new MatTableDataSource<PeriodicElement>(res['message']);
-        // this.dataSource.paginator = this.paginator;
+
+        this.MyDataSource = new MatTableDataSource();
+        this.MyDataSource.data = res["message"];
+        this.page.totalElements = res['totalElement'];
+        this.MyDataSource.paginator = this.paginator;
+        console.log(this.MyDataSource.data);
       }
-    });
+    },
+      error => {
+        console.log('There was an error while retrieving Data !!!' + error);
+      });
   }
+
+  ngAfterViewInit() { }
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {  name: 'Hydrogen' },
-  {  name: 'Helium' },
-  {  name: 'Lithium' },
-  {  name: 'Beryllium' },
-  {  name: 'Boron' },
-  {  name: 'Carbon' },
-  {  name: 'Nitrogen' },
-  {  name: 'Oxygen' },
-  {  name: 'Fluorine' },
-  {  name: 'Neon' },
-  {  name: 'Sodium' },
-  {  name: 'Magnesium' },
-  {  name: 'Aluminum' },
-  {  name: 'Silicon' },
-  {  name: 'Phosphorus' },
-  {  name: 'Sulfur' },
-  {  name: 'Chlorine' },
-  {  name: 'Argon' },
-  {  name: 'Potassium' },
-  {  name: 'Calcium' },
-];
